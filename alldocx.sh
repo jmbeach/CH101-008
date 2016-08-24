2ba309b8553f4eeaa04a0a215900bf1dcc218e10
jmbeach <jaredbeachdesign@gmail.com>
2016-08-24 15:23:56 -0500

    week2day2

--- alldocx.sh
+++ alldocx.sh
@@ -1,4 +1,10 @@
-for file in ./_site/2016/*/*/*
+for d in _site/2016/*/*/
 do
-  ./gendocx.sh $file
+	cd $d
+	for f in ./* 
+	do
+		echo $f
+		~/code/Websites/CH101-008/gendocx.sh $f
+	done
+	cd ~/code/Websites/CH101-008/
 done
